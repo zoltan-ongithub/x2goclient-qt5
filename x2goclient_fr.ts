970cb34e3ee63d23c3778a273f2a1fbf7e6b9c92
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2012-02-18 16:16:05 +0100

    typo fix in x2goclient_fr.ts

--- x2goclient_fr.ts
+++ x2goclient_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" lanuage="fr">
+<TS version="2.0" language="fr">
 <context>
     <name>BrokerPassDialogUi</name>
     <message>
