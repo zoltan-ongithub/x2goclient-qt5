8418860c23c6079f9a49b2bcb1ba7d3712c6d1d9
Oleksandr Shneyder <o.shneyder@phoca-gmbh.de>
2012-12-14 14:30:20 +0100

    Update license headers

--- configdialog.cpp
+++ configdialog.cpp
@@ -1,14 +1,20 @@
-//
-// C++ Implementation: configdialog
-//
-// Description:
-//
-//
-// Author: Oleksandr Shneyder <oleksandr.shneyder@obviously-nice.de>, (C) 2006-2012
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/**************************************************************************
+*   Copyright (C) 2005-2012 by Oleksandr Shneyder                         *
+*   o.shneyder@phoca-gmbh.de                                              *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+***************************************************************************/
+
 #include <QLineEdit>
 #include <QCheckBox>
 #include <QSpinBox>
--- wapi.cpp
+++ wapi.cpp
@@ -1,3 +1,20 @@
+/**************************************************************************
+*   Copyright (C) 2005-2012 by Oleksandr Shneyder                         *
+*   o.shneyder@phoca-gmbh.de                                              *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+***************************************************************************/
+
 #ifndef _WIN32_WINDOWS
 #define _WIN32_WINDOWS 0x0500
 #define _WIN32_WINNT 0x0500
