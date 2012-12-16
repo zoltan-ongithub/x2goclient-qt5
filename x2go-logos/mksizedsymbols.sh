a5c43b9dc799c4064b9eb70fee43f49938d49366
Heinz-M. Graesing <heinz-m.graesing@obviously-nice.de>
2012-12-16 11:34:29 +0100

    Update refurbished X2Go Logo set. License for the X2Go Logos is GPL-2.0+. The inner X2Go logo background is now white (non-transparent) which should fix poor display results for X2Go icons in application menus using a dark theme. (Closes upstream issue: #59).

---
+++ x2go-logos/mksizedsymbols.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Use this script to build typical iconsized PNGs based on a svg sourcefile.
+# ----------------------------------------------------------------------------
+# Written by Heinz-M. Graesing <heinz-m.graesing@obviously-nice.de>
+# (c) 2012 Heinz-M. Graesing GNU GPL v2.0+
+# Last updated on: Dez/01/2012 by Heinz-M. Graesing
+# ----------------------------------------------------------------------------
+
+usage="mksizedsymbols.sh -f sourcefile.svg"
+sizes=( 12 16 32 48 64 128 )
+
+
+counter="0"
+while getopts "f:h" options; do
+	case $options in
+	f ) if [ -f $OPTARG ] ;then
+		file=$OPTARG
+			((counter+=1))
+		fi
+		;;
+	h ) echo $usage
+		exit 0
+		;;
+	\? ) echo $usage
+		exit 1;;
+	esac
+done
+
+
+if [ -e "$file" ];then
+	for i in "${sizes[@]}"
+		do
+			echo $i
+		inkscape --without-gui --export-area-page --export-width=$i --export-height=$i  --file=$file --export-png=${file%.*}-$i.png
+		done
+	exit 0
+else
+	echo $usage
+	exit 1
+fi
+
