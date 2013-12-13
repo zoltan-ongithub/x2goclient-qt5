zoltan-ongithub/x2goclient-qt5

commit c121b7e2d3d83abdc2d7a29637bc3294e38b2ec3

Perform sanity checks on data that comes in from X2Go Servers. Prohibit the execution of arbitrary code via the ~/.bashrc file. (Fixes: #333).

sshprocess.cpp
@@ -20,6 +20,7 @@
 #include "sshmasterconnection.h"
 #include "sshprocess.h"
 #include <QTimer>
+#include <QUuid>
 
 #ifndef Q_OS_WIN
 #include <arpa/inet.h>
@@ -128,14 +129,17 @@ void SshProcess::tunnelLoop()
 
 void SshProcess::startNormal(const QString& cmd)
 {
-    QString shcmd = "sh -c \""+cmd+"\"";
-// #ifdef DEBUG
+    QUuid uuid = QUuid::createUuid();
+    QString uuidStr = uuid.toString().mid(1, 36).toLower();
+
+    QString shcmd = "sh -c \"echo X2GODATABEGIN:" + uuidStr + " && "+cmd+" && echo X2GODATAEND:" + uuidStr +"\";";
+//#ifdef DEBUG
 // ONLY UNCOMMENT FOR TESTING, MIGHT REVEAL PASSWORD WHEN command=RDP
 //    x2goDebug<<"executing remote command: "<<shcmd<<endl;
 // #endif
-    masterCon->addChannelConnection(this, shcmd);
+    masterCon->addChannelConnection(this, uuidStr, shcmd);
     connect(masterCon,SIGNAL(stdOut(SshProcess*,QByteArray)),this,SLOT(slotStdOut(SshProcess*,QByteArray)));
-    connect(masterCon,SIGNAL(channelClosed(SshProcess*)), this,SLOT(slotChannelClosed(SshProcess*)));
+    connect(masterCon,SIGNAL(channelClosed(SshProcess*,QString)), this,SLOT(slotChannelClosed(SshProcess*,QString)));
 }
 
 void SshProcess::start_cp(QString src, QString dst)
@@ -216,7 +220,7 @@ void SshProcess::slotReverseTunnelOk(SshProcess* creator)
 }
 
 
-void SshProcess::slotChannelClosed(SshProcess* creator)
+void SshProcess::slotChannelClosed(SshProcess* creator, QString uuid)
 {
     if (creator!=this)
         return;
@@ -235,8 +239,13 @@ void SshProcess::slotChannelClosed(SshProcess* creator)
             x2goDebug<<"have only stderr, something must be wrong"<<endl;
 #endif
         }
-        else
-            output=stdOutString;
+        else {
+            QString begin_marker = "X2GODATABEGIN:"+uuid+"\n";
+            QString end_marker = "X2GODATAEND:"+uuid+"\n";
+            int output_begin=stdOutString.indexOf(begin_marker) + begin_marker.length();
+            int output_end=stdOutString.indexOf(end_marker);
+            output = stdOutString.mid(output_begin, output_end-output_begin);
+        }
     }
 #ifdef DEBUG
     x2goDebug<<"ssh finished:"<<normalExited<<" - "<<output<<endl;

sshmasterconnection.cpp
@@ -903,7 +903,7 @@ void SshMasterConnection::addChannelConnection ( SshProcess* creator, int sock, 
 }
 
 
-void SshMasterConnection::addChannelConnection ( SshProcess* creator, QString cmd )
+void SshMasterConnection::addChannelConnection ( SshProcess* creator, QString uuid, QString cmd )
 {
 
     ChannelConnection con;
@@ -911,6 +911,7 @@ void SshMasterConnection::addChannelConnection ( SshProcess* creator, QString cm
     con.sock=-1;
     con.creator=creator;
     con.command=cmd;
+    con.uuid=uuid;
 
     channelConnectionsMutex.lock();
     channelConnections<<con;
@@ -1407,7 +1408,8 @@ void SshMasterConnection::finalize ( int item )
         close ( tcpSocket );
     }
     SshProcess* proc=channelConnections[item].creator;
+    QString uuid=channelConnections[item].uuid;
     channelConnections.removeAt ( item );
-    emit channelClosed ( proc );
+    emit channelClosed ( proc, uuid );
 }
 

sshmasterconnection.h
@@ -39,6 +39,7 @@ struct ChannelConnection
     QString forwardHost;
     QString localHost;
     QString command;
+    QString uuid;
     bool operator==(ChannelConnection& t)
     {
         return (channel==t.channel);
@@ -67,7 +68,7 @@ public:
     static void finalizeLibSsh();
     void addChannelConnection(SshProcess* creator, int sock, QString forwardHost,
                               int forwardPort, QString localHost, int localPort, void* channel=0l);
-    void addChannelConnection(SshProcess* creator, QString cmd);
+    void addChannelConnection(SshProcess* creator, QString uuid, QString cmd);
     void addCopyRequest(SshProcess* creator, QString src, QString dst);
     void writeKnownHosts(bool);
     void setKeyPhrase(QString);
@@ -191,7 +192,7 @@ signals:
     void ioErr(SshProcess* caller, QString error, QString lastSessionError);
     void copyErr(SshProcess* caller, QString error, QString lastSessionError);
     void copyOk(SshProcess* caller);
-    void channelClosed(SshProcess* caller);
+    void channelClosed(SshProcess* caller, QString uuid);
 
     void connectionError(QString message, QString lastSessionError);
     void serverAuthError(int errCode, QString lastSessionError, SshMasterConnection*);

sshprocess.h
@@ -77,7 +77,7 @@ private slots:
     void slotStdErr(SshProcess* creator, QByteArray data);
     void slotStdOut(SshProcess* creator, QByteArray data);
     void slotIOerr(SshProcess* creator,QString message, QString sshSessionErr);
-    void slotChannelClosed(SshProcess* creator);
+    void slotChannelClosed(SshProcess* creator, QString uuid);
     void slotReverseTunnelOk(SshProcess* creator);
     void slotCopyOk(SshProcess* creator);
     void slotCopyErr(SshProcess* creator,QString message, QString sshSessionErr);

commit d1bd66113a64e188083f32ef00164c82be3d6e6d

c121b7e2d3d83abdc2d7a29637bc3294e38b2ec3 broke checking if remote command produce only stderr and not stdout. It made x2goclient crash if x2gostartagent send LIMIT error. Current commit fixes this issue.

sshprocess.cpp
@@ -276,7 +276,7 @@ void SshProcess::startTunnel(const QString& forwardHost, uint forwardPort, const
 #ifdef Q_OS_WIN
         QString sshString="plink -batch -P "+
 #else
-	        QString sshString=QString::null+"ssh"+ KEEPALIVE_OPTION +"-o GSSApiAuthentication=yes -o PasswordAuthentication=no -p "+
+        QString sshString=QString::null+"ssh"+ KEEPALIVE_OPTION +"-o GSSApiAuthentication=yes -o PasswordAuthentication=no -p "+
 #endif
                           QString::number(masterCon->getPort())+" "+
                           masterCon->getUser()+"@"+
@@ -374,21 +374,18 @@ void SshProcess::slotChannelClosed(SshProcess* creator, QString uuid)
     }
     else
     {
-        if ( stdOutString.length()<=0 &&  stdErrString.length() >0 )
+        QString begin_marker = "X2GODATABEGIN:"+uuid+"\n";
+        QString end_marker = "X2GODATAEND:"+uuid+"\n";
+        int output_begin=stdOutString.indexOf(begin_marker) + begin_marker.length();
+        int output_end=stdOutString.indexOf(end_marker);
+        output = stdOutString.mid(output_begin, output_end-output_begin);
+        if ( output.length()<=0 &&  stdErrString.length() >0 )
         {
             normalExited=false;
             output=stdErrString;
 #ifdef DEBUG
             x2goDebug<<"have only stderr, something must be wrong"<<endl;
 #endif
-        }
-        else
-        {
-            QString begin_marker = "X2GODATABEGIN:"+uuid+"\n";
-            QString end_marker = "X2GODATAEND:"+uuid+"\n";
-            int output_begin=stdOutString.indexOf(begin_marker) + begin_marker.length();
-            int output_end=stdOutString.indexOf(end_marker);
-            output = stdOutString.mid(output_begin, output_end-output_begin);
         }
     }
 #ifdef DEBUG