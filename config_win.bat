3d76d001c18fef4f88cb98c598a5841be4d7ee96
Oleksandr Shneyder <o.shneyder@phoca-gmbh.de>
2014-07-08 11:51:05 +0200

    Add x2gohelper to start X2Go Client on Windows and clean child processes if X2Go Client crashes.

--- config_win.bat
+++ config_win.bat
@@ -2,4 +2,7 @@ mingw32-make distclean
 lrelease x2goclient.pro
 set X2GO_CLIENT_TARGET=
 qmake
-
+cd x2gohelper
+mingw32-make distclean
+qmake
+cd ..
--- ongetpass.cpp
+++ ongetpass.cpp
@@ -33,10 +33,7 @@
 #ifndef Q_OS_WIN
 #include <sys/types.h>
 #include <signal.h>
-#else
-#include <iostream>
 #endif
-
 
 #include <QPlastiqueStyle>
 #include <QMessageBox>
@@ -45,54 +42,8 @@
 #include <QProcess>
 #include <QLocalSocket>
 #include "x2gologdebug.h"
-using namespace std;
 
 
-#ifdef Q_OS_WIN
-#include <TlHelp32.h>
-
-void killProcess(DWORD pid)
-{
-    HANDLE handle=OpenProcess(PROCESS_TERMINATE,0,pid);
-    if(!handle)
-    {
-//         qCritical()<<"failed to open process";
-        return;
-    }
-    if(!TerminateProcess(handle,0))
-    {
-//         qCritical()<<"failed to terminate process";
-        return;
-    }
-    CloseHandle(handle);
-//     qCritical()<<pid<<" terminated";
-}
-
-void enumerateFromParent(DWORD pid)
-{
-    HANDLE hndl=CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
-    if(hndl==INVALID_HANDLE_VALUE)
-    {
-        qCritical()<<"failed to get system snapshot";
-        return;
-    }
-    PROCESSENTRY32 pentry;
-    pentry.dwSize = sizeof( PROCESSENTRY32 );
-    if( Process32First(hndl,&pentry))
-    {
-        while(Process32Next(hndl,&pentry))
-        {
-            if(pid==pentry.th32ParentProcessID)
-            {
-                enumerateFromParent(pentry.th32ProcessID);
-//                 qCritical()<<"terminating "<<pentry.th32ProcessID<<":"<<QString::fromWCharArray ( pentry.szExeFile);
-                killProcess(pentry.th32ProcessID);
-            }
-        }
-    }
-    CloseHandle(hndl);
-}
-#endif
 
 int x2goMain ( int argc, char *argv[] )
 {
@@ -150,35 +101,6 @@ int x2goMain ( int argc, char *argv[] )
 #ifdef CFGCLIENT
     else
     {
-#ifdef Q_OS_WIN
-        if(argc <=1)
-        {
-            args=app.arguments();
-        }
-        if(args.count()<2 || args[1].indexOf("--child-process")==-1)
-        {
-            QProcess proc;
-            QString executable=args[0];
-            args.pop_front();
-            args.push_front("--child-process");
-            proc.start(executable, args);
-            if(!proc.waitForStarted(4000))
-            {
-                qCritical()<<"Can't start process";
-                return -1;
-            }
-            DWORD pid=proc.pid()->dwProcessId;
-            while(!proc.waitForFinished(300))
-            {
-                QString err=proc.readAllStandardError();
-                QString out=proc.readAllStandardOutput();
-                std::cerr<<err.toStdString();
-                std::cout<<out.toStdString();
-            }
-            enumerateFromParent(pid);
-            return 0;
-        }
-#endif //Q_OS_WIN
         ONMainWindow* mw = new ONMainWindow;
         mw->show();
         return app.exec();
