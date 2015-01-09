zoltan-ongithub/x2goclient-qt5

commit adb03c67eb27995c7998bb3c853e9b94032d3d40

Add several info/error/debug log message while hunting down #702.

onmainwindow.cpp
@@ -1471,7 +1471,7 @@ void ONMainWindow::closeClient()
 
 void ONMainWindow::closeEvent ( QCloseEvent* event )
 {
-    x2goDebug<<"Close event received.";
+    x2goDebug<<"Close event received."<<endl;
 
     if (trayNoclose && !brokerMode)
     {
@@ -1487,18 +1487,16 @@ void ONMainWindow::closeEvent ( QCloseEvent* event )
 
 void ONMainWindow::hideEvent(QHideEvent* event)
 {
-
-
     QMainWindow::hideEvent(event);
     if (event->spontaneous() && trayMinToTray)
         hide();
 
 }
 
-
-
 void ONMainWindow::trayQuit()
 {
+    x2goDebug<<"Quitting tray icon and closing application."<<endl;
+
     closeClient();
     qApp->quit();
 
@@ -1691,6 +1689,7 @@ void ONMainWindow::slotClosePass()
     if (brokerMode)
     {
         if (!config.brokerAuthenticated)
+            x2goErrorf(15)<<tr("Broker authenication failed!");
             close();
     }
     passForm->hide();
@@ -5589,8 +5588,14 @@ void ONMainWindow::slotProxyFinished ( int,QProcess::ExitStatus )
         delete sshConnection;
         x2goDebug<<"Deleted  SSH connection instance." ;
         sshConnection=0;
-        if ( startHidden || closeDisconnect)
+        if (startHidden)
         {
+            x2goInfof(9) << tr("Closing X2Go Client (because it was started in hidden mode).");
+            close();
+        }
+        else if (closeDisconnect)
+        {
+            x2goInfof(10) << tr("Closing X2Go Client (because --close-disconnect was used on the cmdline).");
             close();
         }
     }
@@ -8960,8 +8965,14 @@ void ONMainWindow::slotCmdMessage ( bool result,QString output,
     if(sshConnection)
         delete sshConnection;
     sshConnection=0;
-    if ( startHidden  || closeDisconnect)
+    if (startHidden)
     {
+        x2goInfof(11) << tr("Closing X2Go Client (because it was started in hidden mode).");
+        close();
+    }
+    else if (closeDisconnect)
+    {
+        x2goInfof(12) << tr("Closing X2Go Client (because --close-disconnect was used on the cmdline).");
         close();
     }
 }

printdialog.cpp
@@ -25,7 +25,7 @@
 PrintDialog::PrintDialog ( QWidget* parent, Qt::WindowFlags f )
 		: QDialog ( parent,f )
 {
-	x2goDebug<<"starting print dialog"<<endl;
+	x2goDebug<<"Starting print dialog."<<endl;
 	ui.setupUi ( this );
 	ui.buttonBox->button ( QDialogButtonBox::Ok )->setText (
 	    tr ( "Print" ) );
@@ -44,6 +44,7 @@ PrintDialog::PrintDialog ( QWidget* parent, Qt::WindowFlags f )
 
 PrintDialog::~PrintDialog()
 {
+	x2goDebug<<"Closing print dialog."<<endl;
 }
 
 

printprocess.cpp
@@ -38,6 +38,7 @@
 PrintProcess::PrintProcess ( QString pdf, QString title, QObject *parent ) :
 		QObject ( parent )
 {
+	x2goDebug<<"Starting print process."<<endl;
 	pdfFile=pdf;
 	pdfTitle=title;
 	parentWidg= ( QWidget* ) parent;
@@ -47,14 +48,21 @@ PrintProcess::PrintProcess ( QString pdf, QString title, QObject *parent ) :
 		return;
 	}
 	if ( viewPdf )
+	{
+		x2goDebug<<"Opening PDF file: "<<pdfFile<<" ("<<pdfTitle<<")"<<endl;
 		QTimer::singleShot ( 100, this, SLOT ( openPdf() ) );
+	}
 	else
+	{
+		x2goDebug<<"Printing PDF file: "<<pdfFile<<" ("<<pdfTitle<<")"<<endl;
 		QTimer::singleShot ( 100, this, SLOT ( print() ) );
+	}
 }
 
 
 PrintProcess::~PrintProcess()
 {
+	x2goDebug<<"Closing print process."<<endl;
 }
 
 
@@ -130,6 +138,7 @@ bool PrintProcess::loadSettings()
 
 void PrintProcess::openPdf()
 {
+	x2goDebug<<"opening/saving PDF..."<<endl;
 	if ( pdfOpen )
 	{
 #ifndef Q_OS_WIN
@@ -138,6 +147,7 @@ void PrintProcess::openPdf()
 #else
 		QString cmd=pdfOpenCmd+"\""+pdfFile+"\"";
 #endif
+		x2goDebug<<"Using PDF viewer command: "<<cmd<<endl;
 		x2goDebug<<cmd;
 		if ( ! QProcess::startDetached ( cmd ) )
 			slot_error ( QProcess::FailedToStart );

commit 0ca3a820e48c64e09568901e5777e0c05672c113

fix missing curly brackets, caused by commit adb03c67e

onmainwindow.cpp
@@ -1689,8 +1689,10 @@ void ONMainWindow::slotClosePass()
     if (brokerMode)
     {
         if (!config.brokerAuthenticated)
+        {
             x2goErrorf(15)<<tr("Broker authenication failed!");
             close();
+        }
     }
     passForm->hide();
     if ( !embedMode )