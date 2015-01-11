71f178db001925a90f098890113383d811b76499
Mike DePaulo <mikedep333@gmail.com>
2015-01-10 23:40:24 -0500

    Fix copy-deps-win32.bat for now by hardcoding WinBuilder VM paths

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -1,15 +1,15 @@
-xcopy /E /Y    ..\x2goclient-contrib\cygwin\20141018-2_bin %1\
-xcopy /E /Y    ..\x2goclient-contrib\libssh\0.6.3-762055b-pageant-support-mingw482_bin\bin\libssh.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\libzip\0.9.3_bin\bin\libzip.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_dw2-1.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\MSVC-DLLs\2008-SP1-9.0.30729.8387-x86 %1\
-xcopy /E /Y    ..\x2goclient-contrib\pulse\5.0-rev18_bin %1\
-xcopy /E /Y    ..\x2goclient-contrib\PuTTY\0.63_bin %1\
-xcopy /E /Y /I ..\x2goclient-contrib\VcXsrv\1.15.2.2-xp+vc2013+x2go1_bin %1\VcXsrv
-xcopy /E /Y    ..\x2goclient-contrib\zlib\1.2.8_bin\zlib1.dll %1\
-xcopy /E /Y    ..\x2goclient-contrib\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\cygwin\20141018-2_bin %1\
+xcopy /E /Y    D:\x2goclient-contrib\libssh\0.6.3-762055b-pageant-support-mingw482_bin\bin\libssh.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\libzip\0.9.3_bin\bin\libzip.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_dw2-1.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\MSVC-DLLs\2008-SP1-9.0.30729.8387-x86 %1\
+xcopy /E /Y    D:\x2goclient-contrib\pulse\5.0-rev18_bin %1\
+xcopy /E /Y    D:\x2goclient-contrib\PuTTY\0.63_bin %1\
+xcopy /E /Y /I D:\x2goclient-contrib\VcXsrv\1.15.2.2-xp+vc2013+x2go1_bin %1\VcXsrv
+xcopy /E /Y    D:\x2goclient-contrib\zlib\1.2.8_bin\zlib1.dll %1\
+xcopy /E /Y    D:\x2goclient-contrib\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %1\
 REM until Win32OpenSSL 1.0.1k comes out
-xcopy /E /Y    ..\..\OpenSSL-Win32\bin\ssleay32.dll %1\
-xcopy /E /Y    ..\..\OpenSSL-Win32\bin\libeay32.dll %1\
+xcopy /E /Y    D:\OpenSSL-Win32\bin\ssleay32.dll %1\
+xcopy /E /Y    D:\OpenSSL-Win32\bin\libeay32.dll %1\
