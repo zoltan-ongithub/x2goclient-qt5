d274bbcd292408122d7c634aafd9b67c19dd6a21
Mike DePaulo <mikedep333@gmail.com>
2015-01-12 23:41:15 -0500

    Win32OpenSSL 1.0.1 actually requires the very original version of the VC 2008 DLLs, I determined this emperically and the website states this. It also requires the manifest.

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -4,7 +4,7 @@ xcopy /E /Y    D:\x2goclient-contrib\libzip\0.9.3_bin\bin\libzip.dll %1\
 xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_dw2-1.dll %1\
 xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %1\
 xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\MSVC-DLLs\2008-SP1-9.0.30729.8387-x86 %1\
+xcopy /E /Y    D:\x2goclient-contrib\MSVC-DLLs\2008-9.0.21022.8-x86 %1\
 xcopy /E /Y    D:\x2goclient-contrib\pulse\5.0-rev18_bin %1\
 xcopy /E /Y    D:\x2goclient-contrib\PuTTY\0.63_bin %1\
 xcopy /E /Y /I D:\x2goclient-contrib\VcXsrv\1.15.2.2-xp+vc2013+x2go1_bin %1\VcXsrv
