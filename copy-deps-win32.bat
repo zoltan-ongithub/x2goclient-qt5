22aa6cad8107c3325f29cedd6c221f3ef33ca0a2
Mike DePaulo <mikedep333@gmail.com>
2015-03-29 15:48:54 -0400

    Windows: Update bundled PuTTY from 0.63 to 0.64.

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -7,7 +7,7 @@ xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-r
 xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %1\
 xcopy /E /Y    D:\x2goclient-contrib\MSVC-DLLs\2008-9.0.21022.8-x86 %1\
 xcopy /E /Y    D:\x2goclient-contrib\pulse\5.0-rev18_bin %1\
-xcopy /E /Y    D:\x2goclient-contrib\PuTTY\0.63_bin %1\
+xcopy /E /Y    D:\x2goclient-contrib\PuTTY\0.64_bin %1\
 xcopy /E /Y /I D:\x2goclient-contrib\VcXsrv\1.15.2.5_bin %1\VcXsrv
 xcopy /E /Y    D:\x2goclient-contrib\zlib\1.2.8_bin\zlib1.dll %1\
 xcopy /E /Y    D:\x2goclient-contrib\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %1\
