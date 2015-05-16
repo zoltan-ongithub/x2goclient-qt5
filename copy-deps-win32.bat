bde9d40425a40a36c1293d2d1c69cd0f91b5b938
Mike DePaulo <mikedep333@gmail.com>
2015-05-16 07:33:48 -0400

    copy-deps-win32: require source paths as args

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -1,16 +1,20 @@
-xcopy /E /Y    D:\x2goclient-contrib\cygwin\20150425-2_bin %1\
-del %1\nxproxy.exe.unstripped %1\libXcomp.a %1\libXcomp.dll.a
-xcopy /E /Y    D:\x2goclient-contrib\libssh\0.7.0-x2go1-mingw482_bin\bin\libssh.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\libzip\0.9.3_bin\bin\libzip.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_dw2-1.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\MSVC-DLLs\2008-9.0.21022.8-x86 %1\
-xcopy /E /Y    D:\x2goclient-contrib\pulse\6.0-11.1_bin %1\
-xcopy /E /Y    D:\x2goclient-contrib\PuTTY\0.64_bin %1\
-xcopy /E /Y /I D:\x2goclient-contrib\VcXsrv\1.17.0.0-1_bin %1\VcXsrv
-xcopy /E /Y    D:\x2goclient-contrib\zlib\1.2.8_bin\zlib1.dll %1\
-xcopy /E /Y    D:\x2goclient-contrib\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %1\
-REM until Win32OpenSSL 1.0.1k comes out
-xcopy /E /Y    D:\OpenSSL-Win32\bin\ssleay32.dll %1\
-xcopy /E /Y    D:\OpenSSL-Win32\bin\libeay32.dll %1\
+if "%~3"=="" (
+	echo "Usage: copy-deps-win32.bat path-to-x2goclient-contrib path-to-OpenSSL-Win32 destination"
+	goto :a
+)
+xcopy /E /Y    %1\cygwin\20150425-2_bin %3\
+del %3\nxproxy.exe.unstripped %3\libXcomp.a %3\libXcomp.dll.a
+xcopy /E /Y    %1\libssh\0.7.0-x2go1-mingw482_bin\bin\libssh.dll %3\
+xcopy /E /Y    %1\libzip\0.9.3_bin\bin\libzip.dll %3\
+xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_dw2-1.dll %3\
+xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %3\
+xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %3\
+xcopy /E /Y    %1\MSVC-DLLs\2008-9.0.21022.8-x86 %3\
+xcopy /E /Y    %1\pulse\6.0-11.1_bin %3\
+xcopy /E /Y    %1\PuTTY\0.64_bin %3\
+xcopy /E /Y /I %1\VcXsrv\1.17.0.0-1_bin %3\VcXsrv
+xcopy /E /Y    %1\zlib\1.2.8_bin\zlib1.dll %3\
+xcopy /E /Y    %1\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %3\
+xcopy /E /Y    %2\bin\ssleay32.dll %3\
+xcopy /E /Y    %2\bin\libeay32.dll %3\
+:a
