d5e9e595cbc134766bbc28fdeef1c5a561800c1a
Mihai Moldovan <ionic@ionic.de>
2015-07-30 04:33:30 +0200

    macbuild.sh: remove libpulse and libpulsecore from libraries to be bundled, as something else will bundle them anyway.

--- macbuild.sh
+++ macbuild.sh
@@ -127,8 +127,6 @@ NXPROXY="nxproxy"
 PULSEAUDIO_BINARIES=( "pulseaudio" "esdcompat" "pacat" "pacmd"      "pactl"
                       "pamon"      "paplay"    "parec" "parecord"   "pasuspender" )
 PULSEAUDIO_LIBRARIES=( "libpulse-simple.0.dylib"
-                       "libpulse.0.dylib"
-                       "libpulsecore-6.0.dylib"
                        "pulse-6.0"
                        "pulseaudio" )
 
