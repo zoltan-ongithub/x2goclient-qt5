77dff6c2e31109001acb1ecf7bc816373dcc61c6
Mihai Moldovan <ionic@ionic.de>
2016-09-24 04:38:51 +0200

    src/onmainwindow.{cpp,h}: reformat createRSAKey () only.

--- src/onmainwindow.h
+++ src/onmainwindow.h
@@ -1220,7 +1220,7 @@ private:
     QString key_type_to_string (key_types key_type);
     std::size_t default_size_for_key_type (key_types key_type);
     QString generateKey (key_types key_type, bool host_key = false);
-    QString createRSAKeyBundle();
+    QString createRSAKey ();
 
 ////////////////plugin stuff////////////////////
 #ifdef CFGPLUGIN
