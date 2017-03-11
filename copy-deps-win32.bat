d5d985652ef00e3880938316f48cc811051b54f6
Mike DePaulo <mikedep333@gmail.com>
2017-03-11 07:52:53 -0500

    Windows: Upgrade PulseAudio from 6.0 to 7.1 Fixes: #1152

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -9,7 +9,7 @@ xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libgcc_s_
 xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++-6.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\MSVC-DLLs\2013-12.0.21005.1-x86\msvcr120.dll %3\ || exit /b  %errorlevel%
-xcopy /E /Y    %1\pulse\6.0-11.1_bin %3\ || exit /b  %errorlevel%
+xcopy /E /Y    %1\pulse\7.1-2.2_bin %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\PuTTY\0.67_bin %3\ || exit /b  %errorlevel%
 xcopy /E /Y /I %1\VcXsrv\1.17.0.0-3_bin %3\VcXsrv || exit /b  %errorlevel%
 xcopy /E /Y    %1\zlib\1.2.8_bin\zlib1.dll %3\ || exit /b  %errorlevel%
