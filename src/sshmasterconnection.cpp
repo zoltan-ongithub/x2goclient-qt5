90a8d540cdc6da8bbb1f40b6457a3843794b1386
Mihai Moldovan <ionic@ionic.de>
2017-11-13 22:33:25 +0100

    src/sshmasterconnection.cpp: error out with a useful error message in case no password has been provided when doing password authentication.

--- src/sshmasterconnection.cpp
+++ src/sshmasterconnection.cpp
@@ -1286,6 +1286,7 @@ bool SshMasterConnection::userAuthWithPass()
             }
 
             if (keyPhrase.isNull ()) {
+                authErrors << "No password provided.";
                 return (ret);
             }
             else {
