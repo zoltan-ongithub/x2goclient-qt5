b0a7b5c89f920b9416de426afcda7f42d5039493
Mike DePaulo <mikedep333@gmail.com>
2018-02-18 23:08:27 -0500

    Windows: Update PuTTY from 0.68 to 0.70

--- copy-deps-win32.bat
+++ copy-deps-win32.bat
@@ -11,7 +11,7 @@ xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libstdc++
 xcopy /E /Y    %1\MinGW-DLLs\i686-4.8.2-release-posix-dwarf-rt_v3-rev3\libwinpthread-1.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\MSVC-DLLs\2013-12.0.21005.1-x86\msvcr120.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\pulse\7.1-2.2_bin %3\ || exit /b  %errorlevel%
-xcopy /E /Y    %1\PuTTY\0.68_bin %3\ || exit /b  %errorlevel%
+xcopy /E /Y    %1\PuTTY\0.70_bin %3\ || exit /b  %errorlevel%
 xcopy /E /Y /I %1\VcXsrv\1.17.0.0-3_bin %3\VcXsrv || exit /b  %errorlevel%
 xcopy /E /Y    %1\zlib\1.2.8_bin\zlib1.dll %3\ || exit /b  %errorlevel%
 xcopy /E /Y    %1\zlib\x86-mingw4-1.2.7-1_bin\bin\libz.dll %3\ || exit /b  %errorlevel%
